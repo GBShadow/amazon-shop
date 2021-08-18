7fcc3ba965c6156c49f7401c94556b80aa29f2dd
GBShadow <gbs.shadow@gmail.com>
2021-08-18 10:34:52 -0300

    first commit

---
+++ src/components/ProductFeed.tsx
@@ -0,0 +1,33 @@
+import { IProduct } from 'pages'
+import Product from './Product'
+
+type ProductFeedProps = {
+  products: IProduct[]
+}
+
+function ProductFeed({ products }: ProductFeedProps) {
+  return (
+    <div
+      className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 
+    xl:grid-cols-4 md:-mt-52 mx-auto"
+    >
+      {products.slice(0, 4).map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+
+      <img src="https://links.papareact.com/dyz" className="md:col-span-full" />
+
+      <div className="md:col-span-2">
+        {products.slice(4, 5).map(product => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+
+      {products.slice(4, products.length).map(product => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  )
+}
+
+export default ProductFeed
---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import Banner from 'components/Banner'
+import Header from 'components/Header'
+import ProductFeed from 'components/ProductFeed'
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
+export type IProduct = {
+  id: number
+  title: string
+  price: number
+  category: string
+  description: string
+  image: string
+}
+
+type HomeProps = {
+  products: IProduct[]
+}
+
+export default function Home({ products }: HomeProps) {
+  console.log(products[0])
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <Header />
+
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const products = await fetch('https://fakestoreapi.com/products').then(
+    response => response.json()
+  )
+
+  return {
+    props: {
+      products,
+    },
+  }
+}
